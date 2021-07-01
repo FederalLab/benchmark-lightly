b20adb177a8165a88672a4c97f2571ef4682ef86
densechen <densechen@foxmail.com>
2021-07-01 22:55:12 +0800

    update

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-import os
 import sys
+
 sys.path.insert(0, '/Users/densechen/code/OpenFed')
 
-from openfed.federated.inform.functional import LRTracker
 from benchmark.models import LogisticRegression
 from benchmark.datasets import get_mnist
-from tqdm import tqdm
 from torchvision.transforms import ToTensor
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from openfed.utils import time_string
-from openfed.unified.step import StepAt
+from openfed.unified.utils import before_upload
+from openfed.unified import StepAt
+from openfed.federated.inform import LRTracker
 import torch.optim as optim
 import torch.nn.functional as F
+import openfed.unified as unified
 import openfed.aggregate as aggregate
 import openfed
-import random
 
 
 # >>> set log level
-openfed.logger.log_level(level="INFO")
+openfed.logger.log_level(level="DEBUG")
 
 # >>> Get default arguments from OpenFed
 args = openfed.parser.parse_args()
 
 epochs = 100
+total_parts = 100
+samples = 10
 
 # >>> Specify an API for building federated learning
 openfed_api = openfed.API(frontend=args.rank > 0)
@@ -36,7 +38,11 @@ openfed_api = openfed.API(frontend=args.rank > 0)
 # Or use the with context to add a sequence of step function to openfed_api automatically.
 with StepAt(openfed_api):
     # In other means, it will train for epochs.
-    openfed.StopAtVersion(max_version=epochs)
+    unified.StopAtVersion(max_version=epochs)
+
+# >>> Add a Dispatch
+dispatch = unified.Dispatch(total_parts=total_parts, samples=samples)
+openfed_api.replace_step(before_upload, dispatch)
 
 # Build Network
 net = LogisticRegression(784, 10)
@@ -47,13 +53,16 @@ optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
 # Define aggregator (actually, this is only used for server end): FedAvg, ElasticAggregator
 aggregator = aggregate.AverageAggregator(net.parameters())
 
+# >>> Add an auto-reducer to compute task info
+auto_reducer = aggregate.AutoReducer(weight_key='instances')
+aggregator.register_reducer(auto_reducer)
+
 # >>> Set optimizer and aggregator for federated learning.
 openfed_api.set_aggregator_and_optimizer(aggregator, optimizer)
 
 # Define a LRScheduler
 lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
-    optimizer, T_max=epochs,
-)
+    optimizer, T_max=epochs,)
 
 # >>> Synchronize LR Scheduler between Frontend and Backend
 lr_tracker = LRTracker(lr_scheduler)
@@ -64,8 +73,8 @@ openfed_api.set_state_dict(net.state_dict(keep_vars=True))
 
 # >>> Specify a aggregate trigger
 # It means that every 10 received models will make an aggregate operation.
-aggregate_trigger = openfed.AggregateCount(
-    count=args.world_size-1, checkpoint="/tmp/openfed-model", lr_scheduler=lr_scheduler)
+aggregate_trigger = unified.AggregateCount(
+    count=samples, checkpoint="/tmp/openfed-model", lr_scheduler=lr_scheduler)
 
 # >>> Set the aggregate trigger
 openfed_api.set_aggregate_triggers(aggregate_trigger)
@@ -74,10 +83,10 @@ openfed_api.set_aggregate_triggers(aggregate_trigger)
 openfed_api.build_connection(address=openfed.Address(args=args))
 
 # Load dataset and create dataloader
-train_dataset = get_mnist('data', total_parts=100, train=True)
+train_dataset = get_mnist('data', total_parts=total_parts, train=True)
 test_dataset = MNIST('data', train=False, transform=ToTensor())
 train_dataloader = DataLoader(
-    train_dataset, batch_size=12, shuffle=True, num_workers=0)
+    train_dataset, batch_size=10, shuffle=True, num_workers=0)
 test_dataloader = DataLoader(
     test_dataset, batch_size=1000, shuffle=False, num_workers=0)
 
@@ -90,8 +99,7 @@ with openfed_api:
     # >>> If openfed_api is a frontend, call `run()` will directly skip this function automatically.
     openfed_api.run()
 
-    for i in range(epochs):
-        print(f"{time_string()}: Simulation @{i}")
+    while True:
         # Download latest model.
         print(f"{time_string()}: Downloading latest model from server.")
         if not openfed_api.download():
@@ -101,10 +109,17 @@ with openfed_api:
         # Downloaded
         print(f"{time_string()}: Downloaded!")
 
-        train_dataset.set_part_id(
-            random.randint(0, train_dataset.total_parts-1))
-        process = tqdm(train_dataloader)
-        for data in process:
+        task_info = openfed_api.get_task_info()
+        part_id, version = task_info.get_info(
+            'part_id'), task_info.get_info('version')
+
+        print(f"{time_string()}: part_id={part_id}, version={version}")
+
+        train_dataset.set_part_id(part_id)
+
+        # Train
+        total_loss = []
+        for data in train_dataloader:
             input, target = data
 
             # Start a standard forward/backward pass.
@@ -114,7 +129,28 @@ with openfed_api:
             loss.backward()
 
             optimizer.step()
-            process.set_description(f"lr: {lr_scheduler.get_last_lr()[0]:.5f}, loss: {loss:.2f}")
+            total_loss.append(loss.item())
+        total_loss = sum(total_loss)/len(total_loss)
+
+        # Test
+        correct = []
+        for data in test_dataloader:
+            input, target = data
+
+            # Start a standard forward inference pass.
+            predict = net(input).max(1, keepdim=True)[1]
+            correct.append(predict.eq(target.view_as(
+                predict)).sum().item() / len(target))
+        accuracy = sum(correct) / len(correct)
+
+        task_info.add_info("instances", len(train_dataloader.dataset))
+        task_info.add_info("loss", total_loss)
+        task_info.add_info("accuracy", accuracy)
+        task_info.add_info("version", version + 1)
+
+        # Set task info
+        openfed_api.set_task_info(task_info)
+        openfed_api.update_version(version + 1)
 
         # Upload trained model
         print(f"{time_string()}: Uploading trained model to server.")
@@ -122,9 +158,6 @@ with openfed_api:
             print("Uploading failed.")
             break
         print(f"{time_string()}: Uploaded!")
-
-        # >>> Update inner model version
-        openfed_api.update_version()
 
 # >>> Finished
 openfed_api.finish()

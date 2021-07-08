e5fa7f7fe3c02f927102dd0a15d6a0a1d2901297
densechen <densechen@foxmail.com>
2021-07-08 19:15:27 +0800

    update

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import os
-import sys
-from glob import glob
-
-import openfed
-import openfed.api as of_api
-import openfed.container as fed_container
-import openfed.data as fed_data
-import openfed.pipe as fed_pipe
-import torch
-from openfed.common.task_info import TaskInfo
-from openfed.core.inform import LRTracker
-from openfed.utils import time_string
-from torch.utils.data import DataLoader
-
+from benchmark.utils import StoreDict
+from benchmark.reducer import AutoReducerTb, AutoReducerWanb
 from benchmark.datasets import (get_cifar100, get_emnist, get_mnist,
                                 get_shakespeare_ncp, get_shakespeare_nwp,
                                 get_stackoverflow_nwp, get_stackoverflow_tp)
-from benchmark.reducer import AutoReducerWanb, AutoReducerTb
-from benchmark.utils import StoreDict
+from torch.utils.data import DataLoader
+from openfed.utils import time_string
+from openfed.core.inform import LRTracker
+from openfed.common.task_info import TaskInfo
+import torch
+import openfed.pipe as fed_pipe
+import openfed.data as fed_data
+import openfed.container as fed_container
+import openfed.api as of_api
+import openfed
+from glob import glob
+import os
+import sys
 
 sys.path.insert(0, "/Users/densechen/code/OpenFed")
 
@@ -30,143 +28,143 @@ parser = openfed.parser
 
 # >>> dataset related
 parser.add_argument("--dataset",
-                    type=str,
-                    default='mnist',
-                    choices=['cifar100', 'emnist', 'mnist',
+                    type    = str,
+                    default = 'mnist',
+                    choices = ['cifar100', 'emnist', 'mnist',
                              'shakespeare', 'stackoverflow'],
-                    help='The dataset to train. `cifar100` is copied from TTF,'
+                    help = 'The dataset to train. `cifar100` is copied from TTF,'
                          '`mnist` can be used with different partition methods,'
                          '`emnist` is a federated dataset for image classification,'
                          '`shakespear` and `stackoverflow` are the federated datasets about nlp.'
                     )
 parser.add_argument('--data_root',
-                    type=str,
-                    default="data",
-                    help='The folder contains all datasets.')
+                    type    = str,
+                    default = "data",
+                    help    = 'The folder contains all datasets.')
 parser.add_argument('--download',
-                    action='store_true',
-                    default=False,
-                    help='Whether to download dataset if it is not existing.')
+                    action  = 'store_true',
+                    default = False,
+                    help    = 'Whether to download dataset if it is not existing.')
 parser.add_argument('--partition',
-                    type=str,
-                    default='iid',
-                    choices=['iid', 'dirichlet', 'power-law'],
-                    help='How to split the dataset into different parts.'
+                    type    = str,
+                    default = 'iid',
+                    choices = ['iid', 'dirichlet', 'power-law'],
+                    help    = 'How to split the dataset into different parts.'
                          'Only be used with not federated dataset, such as mnist.'
                     )
 parser.add_argument('--num_parts',
-                    type=int,
-                    default=100,
-                    help='The number of the parts to split into.')
+                    type    = int,
+                    default = 100,
+                    help    = 'The number of the parts to split into.')
 
 # >>> train related
 parser.add_argument('--epochs',
-                    type=int,
-                    default=1,
-                    help='The epochs trained on local client.')
+                    type    = int,
+                    default = 1,
+                    help    = 'The epochs trained on local client.')
 parser.add_argument('--rounds',
-                    type=int,
-                    default=10,
-                    help='The total rounds for federated training.')
+                    type    = int,
+                    default = 10,
+                    help    = 'The total rounds for federated training.')
 parser.add_argument('--samples',
-                    type=int,
-                    default=None,
-                    help='The number of parts used to train at each round.')
+                    type    = int,
+                    default = None,
+                    help    = 'The number of parts used to train at each round.')
 parser.add_argument('--sample_ratio',
-                    type=float,
-                    default=None,
-                    help="The portion of parts used to train at each time, in [0, 1]."
+                    type    = float,
+                    default = None,
+                    help    = "The portion of parts used to train at each time, in [0, 1]."
                          "If `samples` set at the same time, this flag will be ignored."
                     )
 parser.add_argument('--test_samples',
-                    type=int,
-                    default=None,
-                    help="The number of parts used to test at each round. If not specified, use full test dataset.")
+                    type    = int,
+                    default = None,
+                    help    = "The number of parts used to test at each round. If not specified, use full test dataset.")
 parser.add_argument('--ft_optim',
                     '--frontend_optimizer',
-                    type=str,
-                    default='sgd',
-                    choices=['adam', 'sgd'],
-                    help='optimizer used in frontend.')
+                    type    = str,
+                    default = 'sgd',
+                    choices = ['adam', 'sgd'],
+                    help    = 'optimizer used in frontend.')
 parser.add_argument('--bk_optim',
                     '--backend_optimizer',
-                    type=str,
-                    default='sgd',
-                    choices=['adam', 'sgd'],
-                    help='optimizer used in backend.')
+                    type    = str,
+                    default = 'sgd',
+                    choices = ['adam', 'sgd'],
+                    help    = 'optimizer used in backend.')
 parser.add_argument('--pipe',
-                    type=str,
-                    default='none',
-                    choices=['none', 'elastic', 'prox', 'scaffold'],
-                    help='The pipe used to regularize training gradients.')
+                    type    = str,
+                    default = 'none',
+                    choices = ['none', 'elastic', 'prox', 'scaffold'],
+                    help    = 'The pipe used to regularize training gradients.')
 parser.add_argument('--agg',
                     '--aggregator',
-                    type=str,
-                    default='average',
-                    choices=['average', 'naive', 'elastic'],
-                    help='The aggregator used to collect models.')
+                    type    = str,
+                    default = 'average',
+                    choices = ['average', 'naive', 'elastic'],
+                    help    = 'The aggregator used to collect models.')
 parser.add_argument('--ft_lr',
                     '--frontend_learning_rate',
-                    type=float,
-                    default=0.1,
-                    help='The learning rate of frontend optimizer.')
+                    type    = float,
+                    default = 0.1,
+                    help    = 'The learning rate of frontend optimizer.')
 parser.add_argument('--bk_lr',
                     '--backend_learning_rate',
-                    type=float,
-                    default=0.1,
-                    help='The learning rate of backend optimizer.')
+                    type    = float,
+                    default = 0.1,
+                    help    = 'The learning rate of backend optimizer.')
 parser.add_argument('--ft_lr_sch',
                     '--frontend_learning_rate_scheduler',
-                    type=str,
-                    default='none',
-                    choices=['none', 'multi_step', 'cosine'],
-                    help='The frontend learning rate scheduler. (shared among all clients.)')
+                    type    = str,
+                    default = 'none',
+                    choices = ['none', 'multi_step', 'cosine'],
+                    help    = 'The frontend learning rate scheduler. (shared among all clients.)')
 parser.add_argument('--bk_lr_sch',
                     '--backend_learning_rate_scheduler',
-                    type=str,
-                    default='none',
-                    choices=['none', 'multi_step', 'cosine'],
-                    help='The backend learning rate scheduler. (used for server update.)')
+                    type    = str,
+                    default = 'none',
+                    choices = ['none', 'multi_step', 'cosine'],
+                    help    = 'The backend learning rate scheduler. (used for server update.)')
 parser.add_argument('--bz',
                     '--batch_size',
-                    type=int,
-                    default=10,
-                    help='The batch size.')
+                    type    = int,
+                    default = 10,
+                    help    = 'The batch size.')
 parser.add_argument('--gpu',
-                    action='store_true',
-                    default=False,
-                    help="Whether to use gpu.")
+                    action  = 'store_true',
+                    default = False,
+                    help    = "Whether to use gpu.")
 
 # >>> log related
 parser.add_argument('--log_level',
-                    type=str,
-                    default='SUCCESS',
-                    choices=['SUCCESS', 'INFO', 'DEBUG', 'ERROR'],
-                    help='The log level of openfed backend.')
+                    type    = str,
+                    default = 'SUCCESS',
+                    choices = ['SUCCESS', 'INFO', 'DEBUG', 'ERROR'],
+                    help    = 'The log level of openfed backend.')
 parser.add_argument('--pretrained',
-                    type=str,
-                    default='',
-                    help='The path to pretrained model.')
+                    type    = str,
+                    default = '',
+                    help    = 'The path to pretrained model.')
 parser.add_argument('--ckpt',
-                    type=str,
-                    default='/tmp/openfed',
-                    help='The folder to save checkpoints.')
+                    type    = str,
+                    default = '/tmp/openfed',
+                    help    = 'The folder to save checkpoints.')
 parser.add_argument('--wandb',
-                    action='store_true',
-                    default=False,
-                    help='Whether to enable wandb.')
+                    action  = 'store_true',
+                    default = False,
+                    help    = 'Whether to enable wandb.')
 parser.add_argument('--tb',
                     '--tensorboard',
-                    action='store_true',
-                    default=False,
-                    help='Whether to enable tensorboard.')
+                    action  = 'store_true',
+                    default = False,
+                    help    = 'Whether to enable tensorboard.')
 
 # >>> Task related
 parser.add_argument('--task',
-                    type=str,
-                    default='cls',
-                    choices=['cls', 'nwp', 'ncp', 'ae', 'tp'],
-                    help='Task to perform experiments.'
+                    type    = str,
+                    default = 'cls',
+                    choices = ['cls', 'nwp', 'ncp', 'ae', 'tp'],
+                    help    = 'Task to perform experiments.'
                          'cls: Image Classification. '
                          'nwp: Next Word Prediction. '
                          'ncp: Next Word Prediction. '
@@ -174,30 +172,37 @@ parser.add_argument('--task',
                          'tp: Tag Prediction. '
                     )
 parser.add_argument('--network',
-                    type=str,
-                    default='lr',
-                    choices=['emnist', 'emnist_dp', 'emnist_lr',
+                    type    = str,
+                    default = 'lr',
+                    choices = ['emnist', 'emnist_dp', 'emnist_lr',
                              'emnist_ae', 'lr', 'shakespeare_ncp', 'stackoverflow_nwp'],
                     help='Which network used to train.')
 parser.add_argument('--network_cfg',
-                    nargs='+',
-                    type=str,
-                    default='',
-                    action=StoreDict,
-                    help='''
-                    In: args1:0.0 args2:"dict(a=1)"
+                    nargs   = '+',
+                    type    = str,
+                    default = '',
+                    action  = StoreDict,
+                    help    = '''
+                    In : args1: 0.0 args2: "dict(a=1)"
                     Out: {'args1': 0.0, arg2: dict(a=1)}
                     ''')
 
+parser.add_argument('--seed',
+                    type    = int,
+                    default = 0,
+                    help    = "Seed for everything.")
+
 args = parser.parse_args()
+
+openfed.utils.seed_everything(args.seed)
 
 # >>> Config dataset
 if args.dataset == "cifar100":
     train_dataset = get_cifar100(root=args.data_root, train=True)
-    test_dataset = get_cifar100(root=args.data_root, train=False)
+    test_dataset  = get_cifar100(root=args.data_root, train=False)
 elif args.dataset == 'emnist':
     train_dataset = get_emnist(root=args.data_root, train=True)
-    test_dataset = get_emnist(root=args.data_root, train=False)
+    test_dataset  = get_emnist(root=args.data_root, train=False)
 elif args.dataset == 'mnist':
     if args.partition == 'iid':
         partitioner = fed_data.IIDPartitioner()
@@ -214,19 +219,19 @@ elif args.dataset == 'mnist':
 elif args.dataset == 'shakespeare':
     if args.task == 'ncp':
         train_dataset = get_shakespeare_ncp(root=args.root, train=True)
-        test_dataset = get_shakespeare_ncp(root=args.root, train=False)
+        test_dataset  = get_shakespeare_ncp(root=args.root, train=False)
     elif args.task == 'nwp':
         train_dataset = get_shakespeare_nwp(root=args.data_root, train=True)
-        test_dataset = get_shakespeare_nwp(root=args.data_root, train=False)
+        test_dataset  = get_shakespeare_nwp(root=args.data_root, train=False)
     else:
         raise NotImplementedError
 elif args.dataset == 'stackoverflow':
     if args.task == 'nwp':
         train_dataset = get_stackoverflow_nwp(root=args.data_root, train=True)
-        test_dataset = get_stackoverflow_nwp(root=args.data_root, train=False)
+        test_dataset  = get_stackoverflow_nwp(root=args.data_root, train=False)
     elif args.task == 'tp':
         train_dataset = get_stackoverflow_tp(root=args.data_root, train=True)
-        test_dataset = get_stackoverflow_tp(root=args.data_root, train=False)
+        test_dataset  = get_stackoverflow_tp(root=args.data_root, train=False)
     else:
         raise NotImplementedError
 else:
@@ -240,7 +245,7 @@ test_dataloader = DataLoader(
 
 # >>> Specify device
 if args.gpu:
-    args.gpu = args.fed_rank % torch.cuda.device_count()
+    args.gpu    = args.fed_rank % torch.cuda.device_count()
     args.device = torch.device(args.gpu)
     torch.cuda.set_device(args.gpu)
 else:
@@ -290,7 +295,7 @@ if os.path.exists(args.pretrained):
 ckpt_files = glob(os.path.join(args.ckpt))
 if len(ckpt_files) == 0:
     print("No checkpoint found.")
-    last_rounds = 0
+    last_rounds = -1
 else:
     versions = [int(n.split('.')[-1]) for n in ckpt_files]
     latest_vertion = max(versions)
@@ -392,29 +397,29 @@ else:
 # >>> Add an auto-reducer to compute task info
 if args.wandb:
     auto_reducer = AutoReducerWanb(
-        weight_key='instances',
-        additional_keys=['version', 'train'],
-        project="OpenFed")
+        weight_key      = 'instances',
+        additional_keys = ['version', 'train'],
+        project         = "OpenFed")
 elif args.tb:
     auto_reducer = AutoReducerTb(
-        weight_key='instances',
-        additional_keys=['version', 'train'],
-        log_dir="tb_log")
+        weight_key      = 'instances',
+        additional_keys = ['version', 'train'],
+        log_dir         = "tb_log")
 else:
     # use default one.
     auto_reducer = fed_container.AutoReducer(
-        weight_key='instances',
-        additional_keys=['version', 'train'])
+        weight_key      = 'instances',
+        additional_keys = ['version', 'train'])
 
 # >>> Specify an API for building federated learning
 openfed_api = openfed.API(
-    frontend=args.fed_rank > 0,
-    state_dict=network.state_dict(keep_vars=True),
-    ft_optimizer=ft_optim,
-    aggregator=aggregator,
-    bk_optimizer=bk_optim,
-    pipe=pipe,
-    reducer=auto_reducer)
+    frontend     = args.fed_rank > 0,
+    state_dict   = network.state_dict(keep_vars=True),
+    ft_optimizer = ft_optim,
+    aggregator   = aggregator,
+    bk_optimizer = bk_optim,
+    pipe         = pipe,
+    reducer      = auto_reducer)
 
 # >>> Register more step functions.
 with openfed_api:
@@ -429,16 +434,16 @@ with openfed_api:
         train_dataset.total_parts * args.sample_ratio)
 
     of_api.Aggregate(
-        count=[samples, test_samples],
-        checkpoint=args.ckpt,
-        lr_scheduler=[ft_lr_sch, bk_lr_sch])
+        count        = [samples, test_samples],
+        checkpoint   = args.ckpt,
+        lr_scheduler = [ft_lr_sch, bk_lr_sch])
 
     of_api.Download()
-    of_api.Dispatch(samples=samples,
-                    parts_list=train_dataset.total_parts,
-                    test_samples=test_samples,
-                    test_parts_list=test_dataset.total_parts,
-                    )
+    of_api.Dispatch(
+        samples         = samples,
+        parts_list      = train_dataset.total_parts,
+        test_samples    = test_samples,
+        test_parts_list = test_dataset.total_parts)
 
     of_api.Terminate(max_version=args.rounds)
 
@@ -477,7 +482,7 @@ def frontend_loop():
                 # Start a standard forward/backward pass.
                 ft_optim.zero_grad()
                 output = network(input)
-                loss = loss_fn(output, target)
+                loss   = loss_fn(output, target)
 
                 loss.backward()
                 if pipe is not None:
@@ -501,16 +506,16 @@ def frontend_loop():
 
                     # Start a standard forward inference pass.
                     output = network(input)
-                    loss = loss_fn(output, target)
+                    loss   = loss_fn(output, target)
                     correct.append(acc_fn(target, output))
                     total_loss.append(loss.item())
 
         total_loss = sum(total_loss)/len(total_loss)
-        accuracy = sum(correct) / len(correct)
+        accuracy   = sum(correct) / len(correct)
 
-        task_info.loss = total_loss
+        task_info.loss     = total_loss
         task_info.accuracy = accuracy
-        task_info.version = version + 1
+        task_info.version  = version + 1
 
         # Set task info
         openfed_api.update_version(version + 1)
